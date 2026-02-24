544c87950c1a234d728842ea061dae4e132e702f
vincent holm <vincent.holm.hv@gmail.com>
2026-02-24 15:44:02 +0100

    fix db

--- apps/api/src/db/mysql.ts
+++ apps/api/src/db/mysql.ts
@@ -1,11 +1,12 @@
 import { createPool } from "mysql2/promise";
+import { env } from "../config/env.js";
 
 const pool = createPool({
-  host: "shinkansen.proxy.rlwy.net",
-  port: 35189,
-  user: "root",
-  password: "KWUfJXmtNJVamQUTnLKEHcpNqAndnfIe",
-  database: "railway",
+  host: env.MYSQL_HOST,
+  port: Number(env.MYSQL_PORT),
+  user: env.MYSQL_USER,
+  password: env.MYSQL_PASSWORD,
+  database: env.MYSQL_DATABASE,
   ssl: {
     rejectUnauthorized: false,
   },
