99d4999d81ebff87d12a54c636e25186ec8cc358
Melvin <melleos@icloud.com>
2026-03-02 11:58:21 +0100

    Correctly serving compiled HTML files from /dist with routing

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,2 +1,64 @@
-import { env } from './config/env.js';
+import 'dotenv/config';
 import pool from './db/mysql.js';
+import express from 'express';
+import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+/* import helmet from 'helmet' */
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+// Get __dirname for static files
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const distPath = path.join(__dirname, '../../web/dist');
+const manifestPath = path.join(distPath, '.vite/manifest.json');
+
+let manifest = {};
+try {
+    manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'));
+    console.log('Manifest loaded:', manifest);
+} catch (error: any) {
+    console.warn(error.message);
+}
+
+/* app.use(helmet({contentSecurityPolicy: false})) */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(distPath));
+
+app.get('/', (_req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'));
+});
+
+app.get('/login', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../../web/dist/login.html'));
+});
+
+app.get('/register', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../../web/dist/register.html'));
+});
+
+app.get('/src', express.static(path.join(distPath, 'src')));
+
+app.listen(PORT, () => {
+    console.log('Server running on port: ', PORT);
+});
+
+export default app;
+/* import { insertUser } from './repositories/users.repo.js';
+
+console.log(process.env.MYSQL_PUBLIC_URL);
+
+const result = await insertUser(pool, {
+    username: 'jerryjohnson',
+    email: 'jerry@gmail.com',
+    password_hash: '3848328423KHFDHCsjkdfhajk',
+});
+
+console.log(result);
+ */
--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+    base: '/', // or ./ for relative paths
     build: {
-        outDir: 'dist',
         emptyOutDir: true,
+        outDir: 'dist',
+        assetsDir: 'assets',
+        manifest: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'src/login.html'),
+                register: resolve(__dirname, 'src/register.html'),
+            },
+        },
     },
 });
