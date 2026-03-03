36844b7f6b2c2d7871f06eab7a34397ef4a76f07
Melvin <melleos@icloud.com>
2026-03-03 15:35:24 +0100

    Success. Register user works from frontend. Just needs to hash password in server

---
+++ apps/web/src/register.ts
@@ -0,0 +1,56 @@
+/* FORM SUBMIT to register user */
+addEventListener('DOMContentLoaded', (event) => {
+    const form = document.querySelector('form');
+
+    form?.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const emailInput = document.querySelector<HTMLInputElement>('input#email');
+        console.log('emailInput:', emailInput);
+
+        const usernameInput = document.querySelector<HTMLInputElement>('input#username');
+        console.log('usernameInput:', usernameInput);
+
+        const passwordInput = document.querySelector<HTMLInputElement>('input#password');
+        console.log(`password: ${passwordInput}`);
+
+        const email = emailInput?.value;
+        const username = usernameInput?.value;
+        const password = passwordInput?.value;
+
+        if (!email || !username || !password) {
+            console.error(`‼️ email, username or password is missing`);
+            return;
+        }
+
+        console.table({
+            username: username,
+            email: email,
+            password_hash: password,
+        });
+
+        try {
+            const response = await fetch('http://localhost:3000/api/users/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    email: email,
+                    password_hash: password,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Registrering misslyckades');
+            }
+
+            const result = await response.json();
+            console.log(`OK: ${result}`);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+        console.log(`All input OK`);
+    });
+});
