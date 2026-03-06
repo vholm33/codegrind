c8b1bddf3cabcb68445914f6b552f917a1d1f318
Elyas.A <33457679+Casual994@users.noreply.github.com>
2026-03-06 02:14:49 +0100

    Category REMOVE added

--- apps/api/src/routes/categories.routes.ts
+++ apps/api/src/routes/categories.routes.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/get', getAllCategoriesController);
 router.post('/add', addCategoriesController);
+router.delete('/remove', addCategoriesController);
 
 export default router;
