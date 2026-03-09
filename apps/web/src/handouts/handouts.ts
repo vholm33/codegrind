e55fd4d859f1cb9598ff146d6ba910844f74dc82
Elyas.A <33457679+Casual994@users.noreply.github.com>
2026-03-09 23:25:54 +0100

    Handout added

--- apps/web/src/handouts/handouts.ts
+++ apps/web/src/handouts/handouts.ts
@@ -1,11 +1,22 @@
 // 1. fetch mot api (om har route) --> name & handout
 
 // 2. rendera beroende på klickad kategori från sidebaren
+document.addEventListener("DOMContentLoaded", async() => {
 
-document.addEventListener("DOMContentLoaded", () => {
-    // 1.
+    const handout = document.getElementById("handout") as HTMLElement
 
-    // 2.
+    const params = new URLSearchParams(window.location.search)
+    const selectedCategory = params.get("category")
 
-    // 3.
+    const response = await fetch("http://localhost:3000/api/categories/get")
+    const json = await response.json()
+
+    const categories = json.data
+    const category = categories.find((c: any) => c.name === selectedCategory)
+
+    if (category) {
+        handout.innerHTML = `<h2>${category.name}</h2> <p>${category.handout}</p>`
+    }
+    console.log('category from url', selectedCategory)
 })
+   
