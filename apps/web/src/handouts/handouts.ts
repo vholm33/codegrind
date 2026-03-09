1b0fc4e24ec6b4565cf1829c58ec167ca34c8f5d
Melvin <melleos@icloud.com>
2026-03-09 13:17:58 +0100

    Uppdarerade sidebar länkar i html

---
+++ apps/web/src/handouts/handouts.ts
@@ -0,0 +1,11 @@
+// 1. fetch mot api (om har route) --> name & handout
+
+// 2. rendera beroende på klickad kategori från sidebaren
+
+document.addEventListener("DOMContentLoaded", () => {
+    // 1.
+
+    // 2.
+
+    // 3.
+})
