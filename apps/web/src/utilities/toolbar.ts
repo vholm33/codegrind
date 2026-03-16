333265279c8b122b44e28556167a2e7f2513c175
Melvin <melleos@icloud.com>
2026-03-16 15:44:06 +0100

    Fixat sidebar ikon spacing

--- apps/web/src/utilities/toolbar.ts
+++ apps/web/src/utilities/toolbar.ts
@@ -8,30 +8,28 @@ export async function initToolbar() {
 
         if (toolbar) {
             toolbar.innerHTML = `
-        <a href="${baseUrl}/">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="size-6"
-            >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                />
-            </svg>
-        </a>
-        <h1 class="text-center text-2xl font-semibold">CodeGrind</h1>
-        <!-- Register: om inte har inlogg  -->
-        <button id="login-btn" class="text-md w-24 justify-self-end rounded-md bg-blue-500 font-semibold">
-            <a id="login-btn-anchor" href="${baseUrl}/src/register.html">Register</a>
-            <!-- <a href="${baseUrl}/src/register.html">Login</a> -->
-            <!-- <a href="${baseUrl}/src/register.html">Logout</a> -->
-        </button>
-    `;
+                <a href="${baseUrl}/">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="size-6"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                        />
+                    </svg>
+                </a>
+                <h1 class="text-center text-2xl font-semibold">CodeGrind</h1>
+                <!-- Register: om inte har inlogg  -->
+                <button id="login-btn" class="text-md w-24 justify-self-end rounded-md bg-blue-500 font-semibold">
+                    <a id="login-btn-anchor" href="${baseUrl}/src/register.html">Register</a>
+                </button>
+            `;
         }
         console.info('await isLoggedIn()');
 
