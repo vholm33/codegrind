e415e254333f2391923de12fba2a6de934ab0f78
Melvin <melleos@icloud.com>
2026-03-18 16:03:47 +0100

    updaterat erDiagram

--- apps/api/src/mysql/routes/codeQuestion.routes.ts
+++ apps/api/src/mysql/routes/codeQuestion.routes.ts
@@ -2,13 +2,12 @@ import express from 'express';
 import {
     // addCodeQuestionController,
     getCodeQuestionController,
-    getAllQuestions
-
+    getAllQuestions,
 } from '../controllers/codeQuestions.controller.js';
 
 const router = express.Router();
 
-// router.post('/add', addCodeQuestionController);
-router.get('/all', getAllQuestions)
-router.get('/getAll', getCodeQuestionController) // kombination med mongoDB
+router.get('/all', getAllQuestions);
+router.get('/getAll', getCodeQuestionController); // kombination med mongoDB
+
 export default router;
