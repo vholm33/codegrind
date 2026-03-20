eafcc36db1b75ba03b3484a3f8814b8d2ee7a654
Melvin <melleos@icloud.com>
2026-03-21 00:25:11 +0100

    lokal connection

---
+++ apps/api/src/mysql/db/mysql.ts
@@ -0,0 +1,34 @@
+import { createPool } from 'mysql2/promise';
+// import { env } from '../config/env.js';
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+/* const pool = createPool({
+    host: env.MYSQL_HOST,
+    port: Number(env.MYSQL_PORT),
+    user: env.MYSQL_USER,
+    password: env.MYSQL_PASSWORD,
+    database: env.MYSQL_DATABASE,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    connectTimeout: 60000,
+    waitForConnections: true,
+    connectionLimit: 10,
+}); */
+const pool = createPool({
+    host: process.env.LOC_MYSQL_HOST ?? '',
+    port: Number(process.env.LOC_MYSQL_PORT) || 3306,
+    user: process.env.LOC_MYSQL_USER || '',
+    password: process.env.LOC_MYSQL_PASSWORD || '',
+    database: 'localCodeGrind',
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    connectTimeout: 60000,
+    waitForConnections: true,
+    connectionLimit: 10,
+});
+
+export default pool;
