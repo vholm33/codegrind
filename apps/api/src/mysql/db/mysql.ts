3ad44a6b5e0bf2dad94f37cca191ce5d69524faa
Melvin <melleos@icloud.com>
2026-03-22 21:06:07 +0100

    Använder railway config för connection, men lägger till lokal mysql config utkommenterat

--- apps/api/src/mysql/db/mysql.ts
+++ apps/api/src/mysql/db/mysql.ts
@@ -1,6 +1,10 @@
 import { createPool } from 'mysql2/promise';
-import { env } from '../config/env.js';
+// import { env } from '../config/env.js';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Railway cloud koppling
 const pool = createPool({
     host: env.MYSQL_HOST,
     port: Number(env.MYSQL_PORT),
@@ -14,5 +18,20 @@ const pool = createPool({
     waitForConnections: true,
     connectionLimit: 10,
 });
+
+// Lokal mysql koppling
+/* const pool = createPool({
+    host: process.env.LOC_MYSQL_HOST ?? '',
+    port: Number(process.env.LOC_MYSQL_PORT) || 3306,
+    user: process.env.LOC_MYSQL_USER || '',
+    password: process.env.LOC_MYSQL_PASSWORD || '',
+    database: 'localCodeGrind',
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    connectTimeout: 60000,
+    waitForConnections: true,
+    connectionLimit: 10,
+}); */
 
 export default pool;
